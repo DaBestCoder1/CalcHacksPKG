c92d48758289b5fa3de890420bb76e4fb8dc7672
DaBestCoder1 <115974665+DaBestCoder1@users.noreply.github.com>
2022-12-04 19:50:43 +1100

    Add files via upload

---
+++ Setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+name='Easy calculation',
+version='1.0.0',
+author='DaBestCoder1',
+author_email='namishreddy01@gmail.com',
+description='To help calculate integers and numbers',
+packages=setuptools.find_packages(),
+classifiers=[
+"Programming language :: Python :: 3",
+"License :: OSI approved :: MIT License",
+"Operating System :: OS Independant",
+]
+)
---
+++ build/lib/PythonPKG/__init__.py
@@ -0,0 +1,30 @@
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def multi(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+def negadd(a,b):
+    return -a + -b 
+
+def negsub(a,b):
+    return -a - -b 
+
+def negmulti(a,b):
+    return -a * -b 
+
+def negdiv(a,b):
+    return -a / -b
+
+
+
+
+
+                   
